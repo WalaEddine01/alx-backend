7b2fbdb6322e7b77cd2702f8f279a8049f582edf
walaeddine01 <walaaeddine33@gmail.com>
2024-05-10 10:43:28 +0200

    I made the changes on 2024-05-10 10:43:28

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module contains the FIFOCache class
+"""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    This is the FIFOCache class
+    """
