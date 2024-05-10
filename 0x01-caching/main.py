6dbf294632119496b07e06731e71a6d6d992b804
walaeddine01 <walaaeddine33@gmail.com>
2024-05-10 13:05:39 +0200

    I made the changes on 2024-05-10 13:05:39

--- 0x01-caching/main.py
+++ 0x01-caching/main.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
-""" 1-main """
-FIFOCache = __import__('1-fifo_cache').FIFOCache
+""" 0-main """
+my_dict = {'a': 1, 'b': 2, 'c': 3}
 
-my_cache = FIFOCache()
-my_cache.put("A", "Hello")
-my_cache.put("B", "World")
-my_cache.put("C", "Holberton")
-my_cache.put("D", "School")
-my_cache.print_cache()
-my_cache.put("E", "Battery")
-my_cache.print_cache()
-my_cache.put("C", "Street")
-my_cache.print_cache()
-my_cache.put("F", "Mission")
-my_cache.print_cache()
+my_dict.pop(list(my_dict.keys())[-1])
+
+print(my_dict)
