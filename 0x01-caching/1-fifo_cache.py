f0b1728570705a9be0876bd3ded93fae494614f2
walaeddine01 <walaaeddine33@gmail.com>
2024-05-10 13:00:56 +0200

    I made the changes on 2024-05-10 13:00:56

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -14,12 +14,22 @@ class FIFOCache(BaseCaching):
         Constructor
         """
         super().__init__()
-    
+
     def put(self, key, item):
         """
         the put method
         """
-        if key is not None and item is not None:
+        if key is not None and item is not None and key not in self.cache_data:
+            if len(self.cache_data) > self.MAX_ITEMS - 1:
+                key_Dis = next(iter(self.cache_data.keys()))
+                self.cache_data.pop(key_Dis)
+                print("DISCARD: {}".format(key_Dis))
             self.cache_data[key] = item
-        elif len(self.cache_data) > self.MAX_ITEMS:
-            self.cache_data[0]
+
+    def get(self, key):
+        """
+        the get method
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
--- 0x01-caching/main.py
+++ 0x01-caching/main.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
-""" 0-main """
-my_dict = {'a': 1, 'b': 2, 'c': 3}
+""" 1-main """
+FIFOCache = __import__('1-fifo_cache').FIFOCache
 
-print(my_dict[0])
+my_cache = FIFOCache()
+my_cache.put("A", "Hello")
+my_cache.put("B", "World")
+my_cache.put("C", "Holberton")
+my_cache.put("D", "School")
+my_cache.print_cache()
+my_cache.put("E", "Battery")
+my_cache.print_cache()
+my_cache.put("C", "Street")
+my_cache.print_cache()
+my_cache.put("F", "Mission")
+my_cache.print_cache()
