87fd2efe971efcb1ff45f48287b138d02002c989
walaeddine01 <walaaeddine33@gmail.com>
2024-05-10 12:30:56 +0200

    I made the changes on 2024-05-10 12:30:56

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -9,3 +9,17 @@ class FIFOCache(BaseCaching):
     """
     This is the FIFOCache class
     """
+    def __init__(self):
+        """
+        Constructor
+        """
+        super().__init__()
+    
+    def put(self, key, item):
+        """
+        the put method
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+        elif len(self.cache_data) > self.MAX_ITEMS:
+            self.cache_data[0]
