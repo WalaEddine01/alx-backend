b4fe276c7521ff943a60d8c549ac64bc70a71ebb
walaeddine01 <walaaeddine33@gmail.com>
2024-05-14 23:03:41 +0200

    I made the changes on 2024-05-14 23:03:41

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app
+"""
+from locale import getlocale
+from flask import Flask, render_template
+from flask_babel import Babel
+from flask import request
+app = Flask(__name__)
+
+
+class Config:
+    """
+    Config class
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def get_index():
+    """
+    The get_index method
+    """
+    return render_template('3-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """
+    this is the get_local method
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
