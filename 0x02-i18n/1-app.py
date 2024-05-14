251263f19446288b7f20a5fe413d3b2d2b6c7056
walaeddine01 <walaaeddine33@gmail.com>
2024-05-14 22:00:04 +0200

    I made the changes on 2024-05-14 22:00:04

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -2,7 +2,7 @@
 """
 Basic Flask app
 """
-from flask import Flask
+from flask import Flask, render_template
 from flask_babel import Babel
 app = Flask(__name__)
 
@@ -18,3 +18,15 @@ class Config:
 
 app.config.from_object(Config)
 babel = Babel(app)
+
+
+@app.route('/')
+def get_index():
+    """
+    The get_index method
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
