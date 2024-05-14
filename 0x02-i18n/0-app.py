aff23b9d0d9b4bc246ae854dfebcfd555ce52a03
walaeddine01 <walaaeddine33@gmail.com>
2024-05-14 19:44:37 +0200

    I made the changes on 2024-05-14 19:44:37

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
+"""
+Basic Flask app
+"""
 from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
+    '''
+    Render a basic template
+    '''
     return render_template("0-index.html")
 
 
