40b282247e3f48389cb291e8f249142787c57bcd
walaeddine01 <walaaeddine33@gmail.com>
2024-05-17 21:43:26 +0200

    I made the changes on 2024-05-17 21:43:26

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -2,8 +2,7 @@
 """
 Basic Flask app
 """
-from locale import getlocale
-from flask import Flask, render_template
+from flask import Flask, render_template, g
 from flask_babel import Babel
 from flask import request
 app = Flask(__name__)
@@ -22,6 +21,30 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user():
+    """
+    get_user method
+    """
+    try:
+        return users.get(int(request.args.get('login_as')))
+    except Exception:
+        return None
+    
+@app.before_request
+def before_request():
+    """
+    before_request method
+    """
+    g.user = get_user()
+
 @babel.localeselector
 def get_locale():
     """
@@ -37,7 +60,7 @@ def get_index():
     """
     The get_index method
     """
-    return render_template('3-index.html')
+    return render_template('5-index.html')
 
 
 if __name__ == '__main__':
